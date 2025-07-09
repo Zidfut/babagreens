232fb05994164232cce7e63f8455d47b288b7697
zidfut <zidz.ua@gmail.com>
2025-07-09 17:50:09 +0300

    added header

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,12 @@
-// Custom Scripts
-
-// document.body.hidden = true // test
-
+const mobileBtn = document.querySelector('[data-mobile-btn]')
+const headerNav = document.querySelector('[data-header-nav]')
+const closeBtn = document.querySelector('[data-close-btn]')
+mobileBtn.addEventListener('click', () => {
+    headerNav.classList.add('active')
+    // mobileBtn.classList.toggle('active')
+    document.body.classList.toggle('no-scroll')
+})
+closeBtn.addEventListener('click', () => {
+    headerNav.classList.remove('active')
+    document.body.classList.remove('no-scroll')
+})
